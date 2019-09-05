65b9af470e50e72c262cedbc9a1da2bba701bf41
Thales Flores <thalesvflores@gmail.com>
2019-09-04 23:16:15 -0300

    user functions to handle availabilitie of request

--- lib/ex_banking/accounts/user.ex
+++ lib/ex_banking/accounts/user.ex
@@ -4,4 +4,23 @@ defmodule ExBanking.Accounts.User do
   defstruct name: nil, balances: []
 
   @type t :: %__MODULE__{name: String.t(), balances: [Balance.t()]}
+
+  @spec available?(user :: String.t()) :: {:error, :too_many_requests_to_user | :user_does_not_exist} | {:ok, true}
+  def available?(user) do
+    case exist?(user) do
+      :undefined -> {:error, :user_does_not_exist}
+      pid -> reached_limit?(pid)
+    end
+  end
+
+  defp exist?(user), do: :global.whereis_name(user)
+
+  defp reached_limit?(pid) do
+    {:message_queue_len, requests} = :erlang.process_info(pid, :message_queue_len)
+
+    case requests > 10 do
+      false -> {:ok, true}
+      _ -> {:error, :too_many_requests_to_user}
+    end
+  end
 end
